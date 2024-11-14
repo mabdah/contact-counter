ffb54d2d4a033fa94c026c29c8d7a495a075b0df
mabdah <mabisha@telerivet.com>
2024-11-14 17:05:57 +0545

    adding comments

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function contactCounter() {
     var table = project.getOrCreateDataTable("Incoming Message Actions");
     var cursor = table.queryRows();
-    
+    //getting all rows from the datatable
     var contactCounts = {};
     while (cursor.hasNext()) {
         var row = cursor.next();
         var contact_id = row.contact_id;
+        //counting the number of entries of the contact 
         if (contactCounts[contact_id]) {
             contactCounts[contact_id] = contactCounts[contact_id] + 1;
         }
@@ -15,6 +16,7 @@ function contactCounter() {
     }
     var maxCount = 0
     for (var contact_id in contactCounts) {
+        //getting the  contact with the maximum count
         if (contactCounts[contact_id] > maxCount) {
             maxCount = contactCounts[contact_id];
             var contact_detail = contact_id;
