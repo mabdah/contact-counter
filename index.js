db24ce6e206d341e8f1e2dfbcbfbf8264611d5d0
mabdah <mabisha@telerivet.com>
2024-11-14 17:18:13 +0545

    necessary code cleaning changes

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 function contactCounter() {
     var table = project.getOrCreateDataTable("Incoming Message Actions");
     var cursor = table.queryRows();
+
     //getting all rows from the datatable
     var contactCounts = {};
+    var maxCount = 0
+
     while (cursor.hasNext()) {
         var row = cursor.next();
         var contact_id = row.contact_id;
+
         //counting the number of entries of the contact 
-        if (contactCounts[contact_id]) {
-            contactCounts[contact_id] = contactCounts[contact_id] + 1;
-        }
-        else {
-            contactCounts[contact_id] = 1;
-        }
-    }
-    var maxCount = 0
-    for (var contact_id in contactCounts) {
+        contactCounts[contact_id] = contactCounts[contact_id] ? contactCounts[contact_id] + 1 : 1
+
         //getting the  contact with the maximum count
         if (contactCounts[contact_id] > maxCount) {
             maxCount = contactCounts[contact_id];
             var contact_detail = contact_id;
         }
-        
     }
     return {
     contact_id: contact_detail,
